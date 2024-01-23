1f016687317cccba704c9a5df6336534086d523f
Roman Kostetskii <roman.kostetskii@sfera.zone>
2024-01-23 23:31:58 +0300

    moved the anonymous function into a separate one, created a separate channel for the result

--- main.go
+++ main.go
@@ -10,12 +10,17 @@ import (
 	"time"
 )
 
+const (
+	totalPorts = 65535
+)
+
 type PortStatus struct {
 	Port int
 	Open bool
 }
 
-func scanPort(hostname string, port int, wg *sync.WaitGroup, resultChan chan PortStatus) {
+func scanPort(hostname string, port int, wg *sync.WaitGroup, resultScanChan chan PortStatus) {
+	wg.Add(1)
 	defer wg.Done()
 
 	var portStatus PortStatus
@@ -29,8 +34,7 @@ func scanPort(hostname string, port int, wg *sync.WaitGroup, resultChan chan Por
 			conn.Close()
 		}
 	}
-
-	resultChan <- portStatus
+	resultScanChan <- portStatus
 }
 
 func isValidHostname(hostname string) bool {
@@ -43,34 +47,37 @@ func isValidHostname(hostname string) bool {
 	return false
 }
 
-func GetOpenPorts(hostname string) []int {
-	var wg sync.WaitGroup
+func printStatusAndGetOpenPortsList(resultScanChan chan PortStatus, resultChan chan []int) {
 	var openPortsMutex sync.Mutex
-
-	totalPorts := 65535
-	wg.Add(totalPorts * 2)
 	openPortsList := make([]int, 0, totalPorts)
-	resultChan := make(chan PortStatus, totalPorts)
+	scannedPorts := make(map[int]struct{}, totalPorts)
+	for portStatus := range resultScanChan {
+		openPortsMutex.Lock()
+		if portStatus.Open {
+			openPortsList = append(openPortsList, portStatus.Port)
+		}
+		scannedPorts[portStatus.Port] = struct{}{}
+		fmt.Printf("\rProgress: %d/%d ports scanned", len(scannedPorts), totalPorts)
+		openPortsMutex.Unlock()
+	}
+	resultChan <- openPortsList
+}
+
+func GetOpenPorts(hostname string) []int {
+	var wgScan sync.WaitGroup
+	resultScanChan := make(chan PortStatus, 1000)
+	resultChan := make(chan []int, 1)
 
 	for port := 1; port <= totalPorts; port++ {
-		go scanPort(hostname, port, &wg, resultChan)
+		go scanPort(hostname, port, &wgScan, resultScanChan)
 	}
 
-	go func() {
-		scannedPorts := make(map[int]struct{}, totalPorts)
-		for portStatus := range resultChan {
-			openPortsMutex.Lock()
-			if portStatus.Open {
-				openPortsList = append(openPortsList, portStatus.Port)
-			}
-			scannedPorts[portStatus.Port] = struct{}{}
-			fmt.Printf("\rProgress: %d/%d ports scanned", len(scannedPorts), totalPorts)
-			openPortsMutex.Unlock()
-			wg.Done()
-		}
-	}()
+	go printStatusAndGetOpenPortsList(resultScanChan, resultChan)
 
-	wg.Wait()
+	wgScan.Wait()
+	close(resultScanChan)
+
+	openPortsList, _ := <-resultChan
 	close(resultChan)
 
 	sort.Ints(openPortsList)
